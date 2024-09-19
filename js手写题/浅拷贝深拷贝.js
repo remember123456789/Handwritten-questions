6ac0250d724c166c4687d4d7de264f402ee1c813
remember <1815366627@qq.com>
2024-09-19 16:47:57 +0800

    feat 深拷贝

--- js手写题/浅拷贝深拷贝.js
+++ js手写题/浅拷贝深拷贝.js
@@ -1,33 +1,71 @@
 // 浅拷贝
-function shallowCopy(obj) {
-    // 判断是否为对象，如果不是就不进行拷贝
+// function shallowCopy(obj) {
+//     // 判断是否为对象，如果不是就不进行拷贝
+//     if (typeof obj !== 'object') {
+//         return
+//     }
+//     //obj可能为数组可能为对象
+//     let result = obj instanceof Array ? [] : {}
+//     // 递归拷贝
+//     for(let key in obj){
+//         if(obj.hasOwnProperty(key)){
+//             result[key]=obj[key]
+//         }
+//     }
+//     return result
+// }
+
+// // 浅拷贝只复制对象的第一层属性，如果属性值是对象或数组，则只复制引用。
+// let obj={
+//     name:'zs',
+//     age:18,
+//     info:{
+//         address:'北京'
+//     }
+// }
+
+
+// let newObj=shallowCopy(obj)
+
+// obj.name='lisi'
+// newObj.info.address='上海'
+// console.log(obj,'obj');
+// console.log(newObj,'newObj');
+
+// 深拷贝
+// 多了一个递归的操作
+
+function deepCopy(obj) {
     if (typeof obj !== 'object') {
         return
     }
-    //obj可能为数组可能为对象
+
     let result = obj instanceof Array ? [] : {}
-    // 递归拷贝
-    for(let key in obj){
-        if(obj.hasOwnProperty(key)){
-            result[key]=obj[key]
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            result[key] = typeof obj[key] == 'object' ? deepCopy(obj[key]) : obj[key]
         }
     }
     return result
 }
 
-// 浅拷贝只复制对象的第一层属性，如果属性值是对象或数组，则只复制引用。
-let obj={
-    name:'zs',
-    age:18,
-    info:{
-        address:'北京'
+let obj = {
+    name: 'zs',
+    age: 18,
+    info: {
+        address: '北京'
     }
 }
 
+let newObj = deepCopy(obj)
 
-let newObj=shallowCopy(obj)
+obj.name = 'lisi'
+newObj.info.address = '上海'
 
-obj.name='lisi'
-newObj.info.address='上海'
-console.log(obj,'obj');
-console.log(newObj,'newObj');
+console.log(obj, 'obj');
+console.log(newObj, 'newObj');
+
+// 深拷贝的会创建一个新对象，两个对象是不同的存储地址，修改一个对象属性，不会影响到另一个对象属性的变化
+// { name: 'lisi', age: 18, info: { address: '北京' } } obj
+// { name: 'zs', age: 18, info: { address: '上海' } } newObj 
