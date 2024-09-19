22e54f0a28e1a5b5fbaa34031dd3bf26b7a5761d
remember <1815366627@qq.com>
2024-09-19 18:00:45 +0800

    feat 解析url参数对象

--- js手写题/浅拷贝深拷贝.js
+++ js手写题/浅拷贝深拷贝.js
@@ -1,36 +1,38 @@
+// hasOwnProperty() 方法返回一个布尔值，
+// 表示对象自有属性（而不是继承来的属性）中是否具有指定的属性。
 // 浅拷贝
-// function shallowCopy(obj) {
-//     // 判断是否为对象，如果不是就不进行拷贝
-//     if (typeof obj !== 'object') {
-//         return
-//     }
-//     //obj可能为数组可能为对象
-//     let result = obj instanceof Array ? [] : {}
-//     // 递归拷贝
-//     for(let key in obj){
-//         if(obj.hasOwnProperty(key)){
-//             result[key]=obj[key]
-//         }
-//     }
-//     return result
-// }
+function shallowCopy(obj) {
+    // 判断是否为对象，如果不是就不进行拷贝
+    if (typeof obj !== 'object') {
+        return
+    }
+    //obj可能为数组可能为对象
+    let result = obj instanceof Array ? [] : {}
+    // 递归拷贝
+    for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+            result[key]=obj[key]
+        }
+    }
+    return result
+}
 
-// // 浅拷贝只复制对象的第一层属性，如果属性值是对象或数组，则只复制引用。
-// let obj={
-//     name:'zs',
-//     age:18,
-//     info:{
-//         address:'北京'
-//     }
-// }
+// 浅拷贝只复制对象的第一层属性，如果属性值是对象或数组，则只复制引用。
+let obj={
+    name:'zs',
+    age:18,
+    info:{
+        address:'北京'
+    }
+}
 
 
-// let newObj=shallowCopy(obj)
+let newObj=shallowCopy(obj)
 
-// obj.name='lisi'
-// newObj.info.address='上海'
-// console.log(obj,'obj');
-// console.log(newObj,'newObj');
+obj.name='lisi'
+newObj.info.address='上海'
+console.log(obj,'obj');
+console.log(newObj,'newObj');
 
 // 深拷贝
 // 多了一个递归的操作
