5ae49fad70647a71d3b125ed974d03f53589b3af
陈东阳 <1815366627@qq.com>
2024-09-22 09:34:22 +0800

    feat 深拷贝

--- js手写题/浅拷贝深拷贝.js
+++ js手写题/浅拷贝深拷贝.js
@@ -41,3 +41,21 @@ const _shallowClone = (target) => {
   // 补全代码
   return Object.assign({}, target);
 };
+
+// 深拷贝
+const _completeDeepClone = (target, map = new Map()) => {
+  if (target === null) return target;
+  if (typeof target !== "object") return target;
+  const constructor = target.constructor;
+  if (/^(Function|RegExp|Date|Map|Set)$/i.test(constructor.name))
+    return new constructor(target);
+  if (map.get(target)) return map.get(target);
+  map.set(target, true);
+  const cloneTarget = Array.isArray(target) ? [] : {};
+  for (prop in target) {
+    if (target.hasOwnProperty(prop)) {
+      cloneTarget[prop] = _completeDeepClone(target[prop], map);
+    }
+  }
+  return cloneTarget;
+};
