c8f5c113de430e0e1ef3aac15d658f6254886eb2
陈东阳 <1815366627@qq.com>
2025-06-07 18:51:37 +0800

    feat css-兄弟选择器

---
+++ css手写题/避免CSS样式冲突的方法.md
@@ -0,0 +1,13 @@
+1.BEM 架构
+.home-page{
+.home-page-title{
+
+    }
+
+}
+2.CSS Scoped
+scoped css 会对当前组件(scope)下所有元素生成唯一的属性或类名，对所有 CSS 规则将携带唯一属性实现作用域的命名保护
+
+3.CSS Modules
+生成 hash 值
+CSS Modules 是一种 CSS 模块化的实现方式，它允许你使用 CSS 模块，将 CSS 模块化，并避免了 CSS 污染。
---
+++ 设计模式/1.工厂模式/1.工厂模式.js
@@ -0,0 +1,31 @@
+/**
+ * 1.工厂模式就是对创建对象的过程进行单独封装
+ */
+//当前这个对象中有4个属性，但是work属性是动态的，根据它的career的不同职业工作内容不同
+function User(name, age, career, work) {
+    this.name = name;
+    this.age = age;
+    this.career = career;
+    this.work = work;
+}
+//我们创建一个工厂函数  在内容进行判断然后创建User实例
+function Factory(name, age, career) {
+    let work;
+    switch (career) {
+        case 'coder':
+            work = '写代码';
+            break;
+        case 'designer':
+            work = '设计';
+            break;
+        case 'manager':
+            work = '管理';  
+            break;
+    }
+
+    return new User(name, age, career, work);
+}
+
+
+let user = Factory('小明', 18, 'designer')
+console.log(user.work)
