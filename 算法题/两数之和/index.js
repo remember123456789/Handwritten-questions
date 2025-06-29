2e0a2c098c60bdf058c290e4a7d4245231c0a6c1
陈东阳 <1815366627@qq.com>
2025-06-29 12:31:17 +0800

    feat 算法-两数之和

---
+++ 算法题/两数之和/index.js
@@ -0,0 +1,12 @@
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+//测试用例
+console.log(twoSum([3, 2, 3], 6)); // 输出: [0, 2]
